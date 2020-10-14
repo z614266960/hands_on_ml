a4679bfdf041c95211f657ca7525b175a052a303
zrj <614266960@qq.com>
2020-10-14 09:44:06 +0800

    chapter2-模型评估

--- chapter2/house.py
+++ chapter2/house.py
@@ -59,7 +59,7 @@ print(corr_matrix['median_house_value'].sort_values(ascending=False))
 
 
 housing = strat_train_set.drop("median_house_value",axis=1)
-housing_labels = strat_test_set["median_house_value"].copy()
+housing_labels = strat_train_set["median_house_value"].copy()
 
 # 处理缺值
 from sklearn.impute import SimpleImputer
@@ -82,5 +82,107 @@ housing_cat_1hot = encoder.fit_transform(housing_cat_encoded.reshape(-1,1))
 
 # 自定义转换器
 from sklearn.base import BaseEstimator,TransformerMixin
-rooms_ix,bedrooms_ix,polulation_ix,household_ix = 3,4,5,6
+rooms_ix,bedrooms_ix,population_ix,household_ix = 3,4,5,6
+class CombinedAttributesAdder(BaseEstimator,TransformerMixin):
+    def __init__(self,add_bedrooms_per_room = True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self,X,y=None):
+        return self
+    def transform(self,X,y=None):
+        rooms_per_household = X[:,population_ix]/X[:,household_ix]
+        population_per_household = X[:,population_ix]/X[:,household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:,bedrooms_ix]/X[:,rooms_ix]
+            return np.c_[X,rooms_per_household,population_per_household,bedrooms_per_room]
+        else:
+            return np.c_[X,rooms_per_household,population_per_household]
+        
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
 
+housing_extra_attribs = pd.DataFrame(
+    housing_extra_attribs,
+    columns=list(housing.columns)+["rooms_per_household", "population_per_household"])
+housing_extra_attribs.head()
+
+# 转换流水线
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy="median")),
+        ('attribs_adder', CombinedAttributesAdder()),
+        ('std_scaler', StandardScaler()),
+    ])
+
+housing_num_tr = num_pipeline.fit_transform(housing_num)
+
+# 多流水线
+from sklearn.compose import ColumnTransformer
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer([
+        ("num", num_pipeline, num_attribs),
+        ("cat", OneHotEncoder(), cat_attribs),
+    ])
+
+housing_prepared = full_pipeline.fit_transform(housing)
+
+# 选择，训练模型
+# 1.线性模型
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+some_data = housing.iloc[:5]
+some_labels = housing_labels.iloc[:5]
+some_data_prepared = full_pipeline.transform(some_data)
+print("Predictions:", lin_reg.predict(some_data_prepared))
+print("Labels:\t\t", list(some_labels))
+
+from sklearn.metrics import mean_squared_error
+
+housing_predictions = lin_reg.predict(housing_prepared)
+lin_mse = mean_squared_error(housing_labels, housing_predictions)
+lin_rmse = np.sqrt(lin_mse)
+
+
+# 2.决策树
+from sklearn.tree import DecisionTreeRegressor
+
+tree_reg = DecisionTreeRegressor()
+tree_reg.fit(housing_prepared, housing_labels)
+housing_predictions = tree_reg.predict(housing_prepared)
+
+tree_mse = mean_squared_error(housing_labels, housing_predictions)
+tree_rmse = np.sqrt(tree_mse)
+print(tree_rmse)
+
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(tree_reg, housing_prepared, housing_labels,
+                         scoring="neg_mean_squared_error", cv=10)
+tree_rmse_scores = np.sqrt(-scores)
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Standard deviation:", scores.std())
+
+display_scores(tree_rmse_scores)
+
+# 3.随机森林
+from sklearn.ensemble import RandomForestRegressor
+
+forest_reg = RandomForestRegressor(random_state=42)
+forest_reg.fit(housing_prepared, housing_labels)
+housing_predictions = forest_reg.predict(housing_prepared)
+forest_mse = mean_squared_error(housing_labels, housing_predictions)
+forest_rmse = np.sqrt(forest_mse)
+
+print(forest_rmse)
+
+from sklearn.model_selection import cross_val_score
+
+forest_scores = cross_val_score(forest_reg, housing_prepared, housing_labels,
+                                scoring="neg_mean_squared_error", cv=10)
+forest_rmse_scores = np.sqrt(-forest_scores)
+
+display_scores(forest_rmse_scores)
