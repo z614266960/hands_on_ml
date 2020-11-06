3534158c2da863164eba511e37decd3169f51152
zrj <614266960@qq.com>
2020-11-06 15:48:36 +0800

    chapter4 finished

--- chapter4/Trianing_Models.py
+++ chapter4/Trianing_Models.py
@@ -84,3 +84,189 @@ sgd_reg=SGDRegressor(n_iter=50,penalty=None,eta0=0.1)
 sgd_reg.fit(X,y.ravel())
 print(sgd_reg.intercept_,sgd_reg.coef_)
 
+
+# 多项式回归
+np.random.seed(42)
+m=100
+X=6*np.random.rand(m,1)-3
+y=0.5*X**2+X+2+np.random.randn(m,1)
+
+plt.scatter(X,y)
+plt.show()
+
+from sklearn.preprocessing import PolynomialFeatures
+
+
+'''
+X_poly 为 X 与 X^2
+PolynomialFeatures 会添加特征值的组合关系：
+例如： degree为3时，不仅添加a,a^2,a^3,b,b^2,b^3,还会添加ab,a^2b,ab^2
+'''
+poly_features=PolynomialFeatures(degree=2,include_bias=False)
+X_poly=poly_features.fit_transform(X)
+
+lin_reg=LinearRegression()
+lin_reg.fit(X_poly,y)
+lin_reg.intercept_,lin_reg.coef_
+
+X_new=np.linspace(-3, 3, 100).reshape(100, 1)
+X_new_poly = poly_features.transform(X_new)
+y_new = lin_reg.predict(X_new_poly)
+plt.plot(X, y, "b.")
+plt.title('Figure 4-13. Polynomial Regression model predictions') # not shown in the book
+plt.plot(X_new, y_new, "r-", linewidth=2, label="Predictions")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.legend(loc="upper left", fontsize=14)
+plt.axis([-3, 3, 0, 10])
+plt.show()
+
+
+# 学习曲线
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+# 绘制train test error曲线
+def plot_learning_curves(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=10)
+    train_errors, val_errors = [], []
+    for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train[:m], y_train_predict))
+        val_errors.append(mean_squared_error(y_val, y_val_predict))
+
+    plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
+    plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
+    plt.legend(loc="upper right", fontsize=14)   # not shown in the book
+    plt.xlabel("Training set size", fontsize=14) # not shown
+    plt.ylabel("RMSE", fontsize=14)              # not shown
+    
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, X, y)
+plt.axis([0, 80, 0, 3])                         # not shown in the book
+plt.show()       
+
+'''
+发现一阶线性模型不能很好拟合，来试试10阶的
+'''
+
+from sklearn.pipeline import Pipeline
+
+polynomial_regression = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+        ("lin_reg", LinearRegression()),
+    ])
+
+plot_learning_curves(polynomial_regression, X, y)
+plt.axis([0, 80, 0, 3])  # not shown
+plt.title('Figure.4-16')
+plt.show()              
+
+
+# 岭回归
+
+from sklearn.linear_model import Ridge
+
+np.random.seed(42)
+m = 20
+X = 3 * np.random.rand(m, 1)
+y = 1 + 0.5 * X + np.random.randn(m, 1) / 1.5
+
+ridge_reg=Ridge(alpha=1,solver="cholesky")
+ridge_reg.fit(X,y)
+ridge_reg.predict([[1.5]])
+
+# 套索回归 lasso
+from sklearn.linear_model import Lasso
+
+lasso_reg=Lasso(alpha=0.1)
+lasso_reg.fit(X,y)
+lasso_reg.predict([[1.5]])
+
+# 弹性回归，介于lasso和ridge之间 l1_ratio对应于混合比率 𝑟
+
+from sklearn.linear_model import ElasticNet
+elastic_net =ElasticNet(alpha=0.1,l1_ratio=0.5)
+elastic_net.fit(X,y)
+elastic_net.predict([[1.5]])
+
+# 早期停止法 误差达到最小时停止训练
+
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 2 + X + 0.5 * X**2 + np.random.randn(m, 1)
+
+X_train, X_val, y_train, y_val = train_test_split(X[:50], y[:50].ravel(), test_size=0.5, random_state=10)
+
+poly_scaler = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=90, include_bias=False)),
+        ("std_scaler", StandardScaler()),
+    ])
+
+X_train_poly_scaled = poly_scaler.fit_transform(X_train)
+X_val_poly_scaled = poly_scaler.transform(X_val)
+
+sgd_reg = SGDRegressor(max_iter=1,
+                       penalty=None,
+                       eta0=0.0005,
+                       warm_start=True,
+                       learning_rate="constant",
+                       random_state=42)
+
+n_epochs = 500
+train_errors, val_errors = [], []
+for epoch in range(n_epochs):
+    sgd_reg.fit(X_train_poly_scaled, y_train)
+    y_train_predict = sgd_reg.predict(X_train_poly_scaled)
+    y_val_predict = sgd_reg.predict(X_val_poly_scaled)
+    train_errors.append(mean_squared_error(y_train, y_train_predict))
+    val_errors.append(mean_squared_error(y_val, y_val_predict))
+
+best_epoch = np.argmin(val_errors)
+best_val_rmse = np.sqrt(val_errors[best_epoch])
+
+plt.annotate('Best model',
+             xy=(best_epoch, best_val_rmse),
+             xytext=(best_epoch, best_val_rmse + 1),
+             ha="center",
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             fontsize=16,
+            )
+
+best_val_rmse -= 0.03  # just to make the graph look better
+plt.plot([0, n_epochs], [best_val_rmse, best_val_rmse], "k:", linewidth=2)
+plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="Validation set")
+plt.plot(np.sqrt(train_errors), "r--", linewidth=2, label="Training set")
+plt.legend(loc="upper right", fontsize=14)
+plt.xlabel("Epoch", fontsize=14)
+plt.ylabel("RMSE", fontsize=14)
+plt.title('Figure 4-20. Early stopping regularization') # not shown in the book
+plt.show()
+
+# 逻辑回归
+
+from sklearn import datasets
+iris=datasets.load_iris()
+list(iris.keys())
+
+X=iris["data"][:,3:] # petal width
+y=(iris["target"]==2).astype(np.int)# 1 if Iris-Virginica, else 0
+
+from sklearn.linear_model import LogisticRegression
+
+log_reg=LogisticRegression()
+log_reg.fit(X,y)
+
+X_new=np.linspace(0,3,1000).reshape(-1,1)
+y_proba=log_reg.predict_proba(X_new)
+plt.plot(X_new,y_proba[:,1],"g-",label="Iris-Virginica")
+plt.plot(X_new, y_proba[:, 0], "b--", linewidth=2, label="Not Iris-Virginica")
+
+
+
+
