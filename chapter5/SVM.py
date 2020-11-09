23a6f2259c464e4a74f80a84ac644e10060610c8
zrj <614266960@qq.com>
2020-11-09 11:14:18 +0800

    svm finished

---
+++ chapter5/SVM.py
@@ -0,0 +1,354 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  7 13:37:30 2020
+
+@author: 61426
+"""
+
+
+import numpy as np
+from sklearn import datasets
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+def plot_svc_decision_boundary(svm_clf, xmin, xmax):
+    w = svm_clf.coef_[0]
+    b = svm_clf.intercept_[0]
+
+    # At the decision boundary, w0*x0 + w1*x1 + b = 0
+    # => x1 = -w0/w1 * x0 - b/w1
+    x0 = np.linspace(xmin, xmax, 200)
+    decision_boundary = -w[0]/w[1] * x0 - b/w[1]
+
+    margin = 1/w[1]
+    gutter_up = decision_boundary + margin
+    gutter_down = decision_boundary - margin
+
+    svs = svm_clf.support_vectors_
+    plt.scatter(svs[:, 0], svs[:, 1], s=180, facecolors='#FFAAAA')
+    plt.plot(x0, decision_boundary, "k-", linewidth=2)
+    plt.plot(x0, gutter_up, "k--", linewidth=2)
+    plt.plot(x0, gutter_down, "k--", linewidth=2)
+    
+
+'''
+线性svm实现软间隔分类，C越大，划分区间的“间隔”越小；C越小，所容纳的异常越多
+过拟合降低C，欠拟合提高C
+'''
+iris=datasets.load_iris()
+X = iris["data"][:,(2,3)] # petal length, petal width
+y = (iris["target"] == 2).astype(np.float64) # Iris-Viginica
+
+svm_clf = Pipeline((
+    ("scaler",StandardScaler()),
+    ("linear_svc",LinearSVC(C=1,loss="hinge")),
+))
+
+svm_clf.fit(X,y)
+print(svm_clf.predict([[5.5,1.7]]))
+
+# 代码生成线性图表
+scaler = StandardScaler()
+svm_clf1 = LinearSVC(C=1, loss="hinge", random_state=42)
+svm_clf2 = LinearSVC(C=100, loss="hinge", random_state=42)
+
+scaled_svm_clf1 = Pipeline([
+        ("scaler", scaler),
+        ("linear_svc", svm_clf1),
+    ])
+scaled_svm_clf2 = Pipeline([
+        ("scaler", scaler),
+        ("linear_svc", svm_clf2),
+    ])
+
+scaled_svm_clf1.fit(X, y)
+scaled_svm_clf2.fit(X, y)
+# Convert to unscaled parameters
+b1 = svm_clf1.decision_function([-scaler.mean_ / scaler.scale_])
+b2 = svm_clf2.decision_function([-scaler.mean_ / scaler.scale_])
+w1 = svm_clf1.coef_[0] / scaler.scale_
+w2 = svm_clf2.coef_[0] / scaler.scale_
+svm_clf1.intercept_ = np.array([b1])
+svm_clf2.intercept_ = np.array([b2])
+svm_clf1.coef_ = np.array([w1])
+svm_clf2.coef_ = np.array([w2])
+
+# Find support vectors (LinearSVC does not do this automatically)
+t = y * 2 - 1
+support_vectors_idx1 = (t * (X.dot(w1) + b1) < 1).ravel()
+support_vectors_idx2 = (t * (X.dot(w2) + b2) < 1).ravel()
+svm_clf1.support_vectors_ = X[support_vectors_idx1]
+svm_clf2.support_vectors_ = X[support_vectors_idx2]
+plt.figure(figsize=(12,3.2))
+plt.subplot(121)
+plt.plot(X[:, 0][y==1], X[:, 1][y==1], "g^", label="Iris-Virginica")
+plt.plot(X[:, 0][y==0], X[:, 1][y==0], "bs", label="Iris-Versicolor")
+plot_svc_decision_boundary(svm_clf1, 4, 6)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.legend(loc="upper left", fontsize=14)
+plt.title("$C = {}$".format(svm_clf1.C), fontsize=16)
+plt.axis([4, 6, 0.8, 2.8])
+
+plt.subplot(122)
+plt.plot(X[:, 0][y==1], X[:, 1][y==1], "g^")
+plt.plot(X[:, 0][y==0], X[:, 1][y==0], "bs")
+plot_svc_decision_boundary(svm_clf2, 4, 6)
+plt.xlabel("Petal length", fontsize=14)
+plt.title("$C = {}$".format(svm_clf2.C), fontsize=16)
+plt.axis([4, 6, 0.8, 2.8])
+plt.show()
+
+# 非线性svm
+
+# 通过增加平方项进行非线性拟合
+
+X1D = np.linspace(-4, 4, 9).reshape(-1, 1)
+X2D = np.c_[X1D, X1D**2]
+y = np.array([0, 0, 1, 1, 1, 1, 1, 0, 0])
+
+plt.figure(figsize=(11, 4))
+
+
+plt.subplot(121)
+plt.grid(True, which='both')
+plt.axhline(y=0, color='k')
+plt.plot(X1D[:, 0][y==0], np.zeros(4), "bs")
+plt.plot(X1D[:, 0][y==1], np.zeros(5), "g^")
+plt.gca().get_yaxis().set_ticks([])
+plt.xlabel(r"$x_1$", fontsize=20)
+plt.axis([-4.5, 4.5, -0.2, 0.2])
+
+
+plt.subplot(122)
+plt.grid(True, which='both')
+plt.axhline(y=0, color='k')
+plt.axvline(x=0, color='k')
+plt.plot(X2D[:, 0][y==0], X2D[:, 1][y==0], "bs")
+plt.plot(X2D[:, 0][y==1], X2D[:, 1][y==1], "g^")
+plt.xlabel(r"$x_1$", fontsize=20)
+plt.ylabel(r"$x_2$", fontsize=20, rotation=0)
+plt.gca().get_yaxis().set_ticks([0, 4, 8, 12, 16])
+plt.plot([-4.5, 4.5], [6.5, 6.5], "r--", linewidth=3)
+plt.axis([-4.5, 4.5, -1, 17])
+
+plt.subplots_adjust(right=1)
+plt.title('Figure 5-5. Adding features to make a dataset linearly separable')# not shown in the book
+plt.show()
+
+# 添加特征是其线性可分离
+from sklearn.datasets import make_moons
+X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
+
+def plot_dataset(X, y, axes):
+    plt.plot(X[:, 0][y==0], X[:, 1][y==0], "bs")
+    plt.plot(X[:, 0][y==1], X[:, 1][y==1], "g^")
+    plt.axis(axes)
+    plt.grid(True, which='both')
+    plt.xlabel(r"$x_1$", fontsize=20)
+    plt.ylabel(r"$x_2$", fontsize=20, rotation=0)
+
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+plt.show()
+
+
+# 画分离图
+
+from sklearn.datasets import make_moons
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+
+polynomial_svm_clf=Pipeline((("poly_features",PolynomialFeatures(degree=3)),
+                            ("scaler",StandardScaler()),
+                            ("svm_clf",LinearSVC(C=10,loss="hinge"))
+                            ))
+
+polynomial_svm_clf.fit(X,y)
+
+def plot_predictions(clf, axes):
+    x0s = np.linspace(axes[0], axes[1], 100)
+    x1s = np.linspace(axes[2], axes[3], 100)
+    x0, x1 = np.meshgrid(x0s, x1s)
+    X = np.c_[x0.ravel(), x1.ravel()]
+    y_pred = clf.predict(X).reshape(x0.shape)
+    y_decision = clf.decision_function(X).reshape(x0.shape)
+    plt.contourf(x0, x1, y_pred, cmap=plt.cm.brg, alpha=0.2)
+    plt.contourf(x0, x1, y_decision, cmap=plt.cm.brg, alpha=0.1)
+
+plot_predictions(polynomial_svm_clf, [-1.5, 2.5, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+plt.title('Figure 5-6. Linear SVM classifier using polynomial features')# not shown in the book 
+plt.show()
+
+# 多项式核函数
+'''
+超参数coef0 控制模型受高次多项式与低次多项式的影响程度。
+欠拟合则增大degree，过拟合减小
+'''
+from sklearn.svm import SVC
+
+poly_kernel_svm_clf = Pipeline([
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=5))
+    ])
+
+poly_kernel_svm_clf.fit(X, y)
+poly100_kernel_svm_clf = Pipeline([
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="poly", degree=10, coef0=100, C=5))
+    ])
+
+poly100_kernel_svm_clf.fit(X, y)
+
+plt.figure(figsize=(11, 4))
+
+plt.subplot(121)
+plot_predictions(poly_kernel_svm_clf, [-1.5, 2.5, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+plt.title(r"$d=3, r=1, C=5$", fontsize=18)
+
+plt.subplot(122)
+plot_predictions(poly100_kernel_svm_clf, [-1.5, 2.5, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+plt.title(r"$d=10, r=100, C=5$", fontsize=18)
+
+plt.show()
+
+# rbf核
+rbf_kernel_svm_clf = Pipeline(( 
+    ("scaler",StandardScaler()),
+    ("svm_clf",SVC(kernel="rbf",gamma=5,C=0.001))
+))
+
+rbf_kernel_svm_clf.fit(X,y)
+
+# rbf绘图
+from sklearn.svm import SVC
+
+gamma1, gamma2 = 0.1, 5
+C1, C2 = 0.001, 1000
+hyperparams = (gamma1, C1), (gamma1, C2), (gamma2, C1), (gamma2, C2)
+
+svm_clfs = []
+for gamma, C in hyperparams:
+    rbf_kernel_svm_clf = Pipeline([
+            ("scaler", StandardScaler()),
+            ("svm_clf", SVC(kernel="rbf", gamma=gamma, C=C))
+        ])
+    rbf_kernel_svm_clf.fit(X, y)
+    svm_clfs.append(rbf_kernel_svm_clf)
+
+plt.figure(figsize=(11, 7))
+
+for i, svm_clf in enumerate(svm_clfs):
+    plt.subplot(221 + i)
+    plot_predictions(svm_clf, [-1.5, 2.5, -1, 1.5])
+    plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+    gamma, C = hyperparams[i]
+    plt.title(r"$\gamma = {}, C = {}$".format(gamma, C), fontsize=16)
+
+plt.show()
+
+
+# SVM回归
+np.random.seed(42)
+m = 50
+X = 2 * np.random.rand(m, 1)
+y = (4 + 3 * X + np.random.randn(m, 1)).ravel()
+
+from sklearn.svm import LinearSVR
+
+svm_reg = LinearSVR(epsilon=1.5, random_state=42)
+svm_reg.fit(X, y)
+
+svm_reg1 = LinearSVR(epsilon=1.5, random_state=42)
+svm_reg2 = LinearSVR(epsilon=0.5, random_state=42)
+svm_reg1.fit(X, y)
+svm_reg2.fit(X, y)
+
+def find_support_vectors(svm_reg, X, y):
+    y_pred = svm_reg.predict(X)
+    off_margin = (np.abs(y - y_pred) >= svm_reg.epsilon)
+    return np.argwhere(off_margin)
+
+svm_reg1.support_ = find_support_vectors(svm_reg1, X, y)
+svm_reg2.support_ = find_support_vectors(svm_reg2, X, y)
+
+eps_x1 = 1
+eps_y_pred = svm_reg1.predict([[eps_x1]])
+
+def plot_svm_regression(svm_reg, X, y, axes):
+    x1s = np.linspace(axes[0], axes[1], 100).reshape(100, 1)
+    y_pred = svm_reg.predict(x1s)
+    plt.plot(x1s, y_pred, "k-", linewidth=2, label=r"$\hat{y}$")
+    plt.plot(x1s, y_pred + svm_reg.epsilon, "k--")
+    plt.plot(x1s, y_pred - svm_reg.epsilon, "k--")
+    plt.scatter(X[svm_reg.support_], y[svm_reg.support_], s=180, facecolors='#FFAAAA')
+    plt.plot(X, y, "bo")
+    plt.xlabel(r"$x_1$", fontsize=18)
+    plt.legend(loc="upper left", fontsize=18)
+    plt.axis(axes)
+
+plt.figure(figsize=(9, 4))
+plt.subplot(121)
+plot_svm_regression(svm_reg1, X, y, [0, 2, 3, 11])
+plt.title(r"$\epsilon = {}$".format(svm_reg1.epsilon), fontsize=18)
+plt.ylabel(r"$y$", fontsize=18, rotation=0)
+#plt.plot([eps_x1, eps_x1], [eps_y_pred, eps_y_pred - svm_reg1.epsilon], "k-", linewidth=2)
+plt.annotate(
+        '', xy=(eps_x1, eps_y_pred), xycoords='data',
+        xytext=(eps_x1, eps_y_pred - svm_reg1.epsilon),
+        textcoords='data', arrowprops={'arrowstyle': '<->', 'linewidth': 1.5}
+    )
+plt.text(0.91, 5.6, r"$\epsilon$", fontsize=20)
+plt.subplot(122)
+plot_svm_regression(svm_reg2, X, y, [0, 2, 3, 11])
+plt.title(r"$\epsilon = {}$".format(svm_reg2.epsilon), fontsize=18)
+plt.show()
+
+# 线性svm分类器作图
+iris = datasets.load_iris()
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = (iris["target"] == 2).astype(np.float64)  # Iris-Virginica
+
+from mpl_toolkits.mplot3d import Axes3D
+
+def plot_3D_decision_function(ax, w, b, x1_lim=[4, 6], x2_lim=[0.8, 2.8]):
+    x1_in_bounds = (X[:, 0] > x1_lim[0]) & (X[:, 0] < x1_lim[1])
+    X_crop = X[x1_in_bounds]
+    y_crop = y[x1_in_bounds]
+    x1s = np.linspace(x1_lim[0], x1_lim[1], 20)
+    x2s = np.linspace(x2_lim[0], x2_lim[1], 20)
+    x1, x2 = np.meshgrid(x1s, x2s)
+    xs = np.c_[x1.ravel(), x2.ravel()]
+    df = (xs.dot(w) + b).reshape(x1.shape)
+    m = 1 / np.linalg.norm(w)
+    boundary_x2s = -x1s*(w[0]/w[1])-b/w[1]
+    margin_x2s_1 = -x1s*(w[0]/w[1])-(b-1)/w[1]
+    margin_x2s_2 = -x1s*(w[0]/w[1])-(b+1)/w[1]
+    ax.plot_surface(x1s, x2, np.zeros_like(x1),
+                    color="b", alpha=0.2, cstride=100, rstride=100)
+    ax.plot(x1s, boundary_x2s, 0, "k-", linewidth=2, label=r"$h=0$")
+    ax.plot(x1s, margin_x2s_1, 0, "k--", linewidth=2, label=r"$h=\pm 1$")
+    ax.plot(x1s, margin_x2s_2, 0, "k--", linewidth=2)
+    ax.plot(X_crop[:, 0][y_crop==1], X_crop[:, 1][y_crop==1], 0, "g^")
+    ax.plot_wireframe(x1, x2, df, alpha=0.3, color="k")
+    ax.plot(X_crop[:, 0][y_crop==0], X_crop[:, 1][y_crop==0], 0, "bs")
+    ax.axis(x1_lim + x2_lim)
+    ax.text(4.5, 2.5, 3.8, "Decision function $h$", fontsize=15)
+    ax.set_xlabel(r"Petal length", fontsize=15)
+    ax.set_ylabel(r"Petal width", fontsize=15)
+    ax.set_zlabel(r"$h = \mathbf{w}^T \mathbf{x} + b$", fontsize=18)
+    ax.legend(loc="upper left", fontsize=16)
+
+fig = plt.figure(figsize=(11, 6))
+ax1 = fig.add_subplot(111, projection='3d')
+plot_3D_decision_function(ax1, w=svm_clf2.coef_[0], b=svm_clf2.intercept_[0])
+plt.title('Figure 5-12. Decision function for the iris dataset')
+
+plt.show()
